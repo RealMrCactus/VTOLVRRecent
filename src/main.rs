d6dec9dcd0eabd4550d04bfc15fc7ce2aed22a03
Bhraedain Teitgen <teitgenbhraedain@gmail.com>
2023-11-15 22:28:39 -0500

    Wait for user to close or press key, write to output.txt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ extern crate regex;
 use regex::Regex;
 use std::fs;
 use std::collections::HashSet;
-
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let app_data_path = std::env::var("APPDATA").expect("APPDATA not found");
@@ -21,18 +22,23 @@ fn main() {
 
     // Find all matches and process them
     let mut found_any = false;
+    let mut output = String::new();
+
     for cap in re.captures_iter(&text) {
         let id = cap[1].to_string();
         let name = cap[2].to_string();
         let team = cap[3].to_string();
-
+    
         if seen.insert((id.clone(), name.clone(), team.clone())) {
             found_any = true;
-            println!("ID: {}, Name: {}, Team: {}", id, name, team);
+            output.push_str(&format!("ID: {}, Name: {}, Team: {}\n", id, name, team));
         }
     }
-
+    
     if !found_any {
-        println!("No player data found in the log.");
+        output.push_str("No player data found in the log.\n");
     }
+    fs::write("output.txt", output.clone()).expect("Unable to write to file");
+    println!("{}", output);
+    std::io::stdin().read_line(&mut String::new()).unwrap();
 }
